b0ed8e5408c5186d4f7f3e7e30e2d12b92ce9cce
Dhananjay Mohokar <dmmohokar94@gmail.com>
2024-10-22 11:35:36 +0530

    "Updated AtlasSearch.css with new styles and AtlasSearch.jsx with rewritten header content and added PASSION and PRUTL framework descriptions."

--- atlasframework/src/Atlas/SearchEngine/AtlasSearch.jsx
+++ atlasframework/src/Atlas/SearchEngine/AtlasSearch.jsx
@@ -12,7 +12,8 @@ const AtlasSearch = () => {
 
   const handleSearch = () => {
     fetch(
-      `https://atlas.passionit.com:8983/solr/research/select?indent=true&q.op=OR&q=${searchQuery}&useParams=`,
+      //  `http://localhost:8983/solr/research/select?indent=true&q.op=OR&q=${searchQuery}&useParams=`,
+      `https://audit.passionit.com:8983/solr/research/select?indent=true&q.op=OR&q=${searchQuery}&useParams=`,
       {}
     )
       .then((response) => response.json())
@@ -102,41 +103,6 @@ const AtlasSearch = () => {
               src={Image}
               alt="Img"
             />
-          </div>
-        </div>
-        <div className="header_div mt-4">
-          <div className="content-section header_container">
-            <h2>What is Data?</h2>
-            <p>
-              Data refers to a collection of raw facts, figures, and statistics
-              that are collected and used for analysis, research, and
-              decision-making. It can come in various forms, such as numbers,
-              text, images, or audio, and is gathered from multiple sources,
-              including experiments, surveys, databases, and observations.
-            </p>
-          </div>
-
-          <div className="content-section header_container">
-            <h2>PASSION Framework:</h2>
-            <h4>Data’s Role Across Dimensions</h4>
-            <div>
-              <p>
-                <strong>Probing:</strong> Data is the starting point for inquiry
-                and exploration...
-              </p>
-            </div>
-          </div>
-
-          <div className="content-section header_container">
-            <h2>PRUTL Dimensions: </h2>
-            <h4>Data's Ethical Impact</h4>
-            <p>
-              <strong>Positive Soul:</strong> Data fosters trust in
-              institutions...
-            </p>
-            <p>
-              Data’s Role in Shaping the Future: Data is not just a commodity...
-            </p>
           </div>
         </div>
       </header>
@@ -303,6 +269,225 @@ const AtlasSearch = () => {
         </div>
 
         {noResults && <p className="no-results">No results found.</p>}
+      </div>
+      <div className="header_div mt-4">
+        <div
+          className="content-section header_container"
+          style={{ lineHeight: 1.8 }}
+        >
+          <h2 style={{ textAlign: "center" }}>PASSION: The Body’s Drive </h2>
+          <div
+            style={{
+              border: "3px solid #e50000",
+              width: "200px",
+              borderRadius: "50px",
+              textAlign: "center",
+              display: "block",
+              margin: "0 auto",
+            }}
+          />
+          <p>
+            PASSION refers to the deep, internal energy that fuels human action
+            and growth, driving individuals to engage with the world and express
+            themselves fully. It reflects a dynamic aspect of human nature that
+            propels us forward in pursuit of goals, discoveries, and
+            accomplishments. PASSION in this context is divided into seven core
+            dimensions that capture the various ways in which humans engage with
+            life and the world:
+          </p>
+          <ul>
+            <li>
+              <strong>Probing:</strong> This dimension of passion relates to
+              curiosity and the need to explore. Humans are naturally
+              inquisitive, driven by the desire to understand the unknown, dig
+              deeper into problems, and ask questions that lead to discovery.
+            </li>
+            <li>
+              <strong>Innovating:</strong> Innovation is the creative aspect of
+              passion. It’s the urge to go beyond the status quo, to create new
+              ideas, solutions, and approaches that challenge existing norms.
+            </li>
+            <li>
+              <strong>Acting:</strong> Passion is expressed in action. Acting is
+              about taking initiative, putting ideas and desires into motion,
+              and making tangible changes in the world through effort and
+              determination.
+            </li>
+            <li>
+              <strong>Scoping:</strong> T Scoping refers to the ability to see
+              the bigger picture. This dimension of passion is about setting
+              one’s sights on the broader vision, understanding context, and
+              determining the scope of one’s endeavors.
+            </li>
+            <li>
+              <strong>Setting:</strong> Setting defines the strategic aspect of
+              passion. It’s about creating plans, establishing frameworks, and
+              setting goals to achieve desired outcomes. This requires
+              foresight, preparation, and careful thought.
+            </li>
+            <li>
+              <strong>Owning:</strong> Ownership is about taking responsibility
+              for one’s actions, choices, and paths. This dimension of passion
+              highlights the importance of accountability and commitment to
+              one's personal mission.
+            </li>
+            <li>
+              <strong>Nurturing:</strong> Passion also involves caring for and
+              cultivating what has been started. Nurturing is the patient and
+              loving development of ideas, relationships, or projects, ensuring
+              they grow and succeed over time.
+            </li>
+          </ul>
+          <p>
+            Together, these seven dimensions make up the PASSION of the body,
+            representing the physical and intellectual forces that drive human
+            behavior and achievement.
+          </p>
+        </div>
+
+        <div className="content-section header_container">
+          <h2 style={{ textAlign: "center" }}>PRUTL: The Soul’s Compass </h2>
+          <div
+            style={{
+              border: "3px solid #e50000",
+              width: "200px",
+              borderRadius: "50px",
+              textAlign: "center",
+              display: "block",
+              margin: "0 auto",
+            }}
+          />
+          <p>
+            PRUTL reflects the soul’s moral and emotional compass, providing a
+            framework for understanding how people relate to others, the world,
+            and themselves. PRUTL is split into two categories: Positive Soul
+            and Negative Soul, each representing contrasting aspects of the
+            human spirit. It helps to define how individuals either elevate or
+            degrade their soul through their actions and choices.
+          </p>
+          <h4>
+            <strong>Positive Soul (PRUTL)</strong>
+          </h4>{" "}
+          The Positive Soul embodies the highest virtues that uplift humanity
+          and foster meaningful connections. These virtues are:
+          <ul>
+            <li>
+              <strong>Peace:</strong>Inner calm and harmony, both within oneself
+              and in relationships with others. Peace is essential for a
+              balanced and centered life.
+            </li>
+            <li>
+              <strong>Respect:</strong>The acknowledgement of the inherent worth
+              and dignity of all people. Respect forms the foundation for
+              empathy, understanding, and fairness in human interactions.
+            </li>
+            <li>
+              <strong>Unity:</strong>The sense of belonging and connection with
+              others. Unity fosters collaboration, cooperation, and a feeling of
+              togetherness that transcends individual differences.
+            </li>
+            <li>
+              <strong>Trust:</strong>Trust is about building faith in others and
+              oneself. It’s a critical element of all relationships, and it
+              supports a stable and secure sense of community.
+            </li>
+            <li>
+              <strong>Love:</strong>The highest form of emotional connection,
+              love nurtures, heals, and binds people together. Love is selfless,
+              kind, and enduring, and it encompasses compassion and
+              understanding for all.
+            </li>
+          </ul>
+          <h4>
+            <strong>Negative Soul (PRUTL)</strong>
+          </h4>{" "}
+          In contrast, the Negative Soul dimensions illustrate the destructive
+          aspects of human nature that can erode one’s moral foundation and
+          relationships with others:
+          <ul>
+            <li>
+              <strong>Pride:</strong>Excessive self-importance that blinds one
+              to the needs or value of others. Pride can lead to arrogance and a
+              lack of empathy.
+            </li>
+            <li>
+              <strong>Rule:</strong>The desire for control and dominance over
+              others. This aspect of the soul focuses on power and authority
+              rather than collaboration and equality.
+            </li>
+            <li>
+              <strong>Usurp:</strong>The act of taking what belongs to others,
+              be it power, possessions, or rights. Usurping is a manifestation
+              of greed and selfish ambition.
+            </li>
+            <li>
+              <strong>Tempt:</strong>The pull toward desires that may lead to
+              unethical behavior. Temptation represents the challenge of staying
+              true to one’s values in the face of immediate gratification.
+            </li>
+            <li>
+              <strong>Lust:</strong>An overwhelming desire for physical or
+              material pleasures at the expense of deeper emotional and
+              spiritual fulfillment. Lust leads to superficial connections and
+              discontent.
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div className="content-section header_container">
+        <h2 style={{ textAlign: "center" }}>
+          PASSION and PRUTL: The Humanitarian Perspective
+        </h2>
+        <div
+          style={{
+            border: "3px solid #e50000",
+            width: "200px",
+            borderRadius: "50px",
+            textAlign: "center",
+            display: "block",
+            margin: "0 auto",
+          }}
+        />
+        <p>
+          By analyzing human actions, behaviors, and even daily interactions
+          through the lens of PASSION and PRUTL, we can better understand the
+          complex balance between body and soul.
+        </p>
+        <ul style={{ lineHeight: 1.8 }}>
+          <li>
+            <strong>PASSION</strong>, representing the body, drives the physical
+            and intellectual aspects of existence, urging individuals to act,
+            create, and explore.
+          </li>
+          <li>
+            <strong>PRUTL</strong>, representing the soul, governs the moral and
+            emotional dimensions, guiding how people relate to one another and
+            the world at large.
+          </li>
+        </ul>
+        <p>
+          In humanitarian terms, the <strong>positive side of PRUTL</strong>{" "}
+          aligns with virtues that promote social harmony, understanding, and
+          love, while <strong>PASSION</strong> motivates progress, innovation,
+          and personal growth. Both must work in concert for a balanced,
+          fulfilling life.
+        </p>
+        <p>
+          However, the negative aspects of PRUTL reveal how the{" "}
+          <strong>misalignment of soul </strong> can lead to harmful
+          consequences. If driven by lust, pride, or usurpation, even the most
+          passionate efforts can be derailed into destructive paths.
+          Understanding these dynamics helps provide insight into human
+          conflicts, personal dilemmas, and the broader social issues we face.
+        </p>
+        <p>
+          By using PASSION and PRUTL as analytical tools,{" "}
+          <strong>Atlas Infopedia </strong>will offer a unique perspective,
+          enabling users to explore the ethical and energetic dimensions behind
+          everything—from words to celebrities to everyday occurrences—shedding
+          light on the deeper humanitarian context that influences our
+          collective experience.
+        </p>
       </div>
     </div>
   );
