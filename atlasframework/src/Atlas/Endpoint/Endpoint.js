30ad03d93160aa12220f3083c78239bf39810cfd
Dhananjay Mohokar <dmmohokar94@gmail.com>
2024-10-30 17:22:08 +0530

    "Removed logo files, updated index.html, and toggled API endpoint URLs in Endpoint.js and AtlasSearch.jsx"

--- atlasframework/src/Atlas/Endpoint/Endpoint.js
+++ atlasframework/src/Atlas/Endpoint/Endpoint.js
@@ -1,5 +1,5 @@
 const BASE_URL = "http://localhost:5018/atlas-api/";
-
+//const BASE_URL = "https://atlas.passionit.com/atlas-api/";
 export const LOGIN = `${BASE_URL}login`;
 export const SIGNUP = `${BASE_URL}signup`;
 export const VERIFY_OTP_API = `${BASE_URL}verify-otp`;
